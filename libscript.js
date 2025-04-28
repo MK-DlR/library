32daa9dff2a1e257e7720a48094bada467c74464
milkteapuppy <156838365+milkteapuppy@users.noreply.github.com>
2025-04-28 13:58:32 -0400

    Complete addBookToLibrary and remove test values
    Complete step 1c; step 1 fully complete
    Remove manually created book test values
    Fix addBookToLibrary functionality

--- libscript.js
+++ libscript.js
@@ -2,34 +2,25 @@
 const myLibrary = [];
 
 // book constructor
-function Book(title, author, pages, read) {
+function Book(title, author, pages, published, read, adaptation) {
   if (!new.target) {
     throw Error("You must use the 'new' operator to call the constructor");
   }
   this.title = title;
   this.author = author;
   this.pages = pages;
+  this.published = published;
   this.read = read;
-  this.uuid = self.crypto.randomUUID();
-  // console.log(this.uuid);
+  this.adaptation = adaptation;
+  this.id = self.crypto.randomUUID();
   this.info = function () {
-    return `${this.title} by ${this.author}, ${this.pages}, ${read}`;
+    return `${this.title} by ${this.author}, ${this.pages}, ${this.read}, ${this.movie}.`;
   };
 }
 
-// add book to library array
-function addBookToLibrary() {
-  myLibrary.push(theHobbit.info()); // temporary test value
-  console.log(myLibrary);
+// add books to library array
+function addBookToLibrary(title, author, pages, published, read, adaptation) {
+  // take params, create a book then store it in the array
+  myLibrary.push(new Book(title, author, pages, published, read, adaptation));
+  console.table(myLibrary);
 }
-
-// example book
-const theHobbit = new Book(
-  "The Hobbit",
-  "J.R.R. Tolkien",
-  "295 pages",
-  "not read yet"
-);
-
-//console.log(theHobbit.info());
-addBookToLibrary(); // output: Array [ "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet" ]
